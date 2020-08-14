c92ffe868ba70d85585db481244bb71189e400c7
Rodrigo Viana <rodrigoviana50@gmail.com>
2020-08-13 22:40:36 -0300

    Começando a incrementar as restrições de senha

--- src/Login.java
+++ src/Login.java
@@ -23,47 +23,88 @@ public class Login {
         System.out.printf("\n");
         System.out.printf("            Digite uma senha: ");
         String senha = scanner.nextLine();
-        System.out.printf("\n");
 
-
-        if (senha.length() >= 8) {
-            System.out.printf("                    Login criado com sucesso");
-
-
-        }
-        else {
-            System.out.printf("         Digite uma senha de no mínimo 8 caracteres: ");
-            String senha1 = scanner.nextLine();
-            System.out.printf("                    Login criado com sucesso");
-
-
-        }
-
-
-        System.out.printf("\n");
-        System.out.printf("\n");
-        System.out.printf("---------------------------------------------------------------------");
-        System.out.printf("\n");
-        System.out.printf("                             Login");
-        System.out.printf("\n");
-        System.out.printf("---------------------------------------------------------------------");
-        System.out.printf("\n");
-        System.out.printf("\n");
-        System.out.printf("            Digite seu Login: ");
-        String log1 = scanner.nextLine();
-        if (log.equals(log1)) {
-            System.out.printf("\n");
-            System.out.printf("            Digite sua senha: ");
-            String senha1 = scanner.nextLine();
-            System.out.printf("\n");
-            if (senha.equals(senha1)) {
-                System.out.printf("                 Login realizado com sucesso");
-            } else {
-                System.out.printf("            senha inválida");
+        int cont = 0;
+        boolean temCaracter = false;
+        boolean temMaiuscula = false;
+        boolean temMinuscula = false;
+        boolean temNumero = false;
+        while(cont < (senha.length()) ) {
+            String caracter = String.valueOf(senha.charAt(cont));
+            if (caracter.equals("@-!-#")) {
+                temCaracter = caracter.equals("@-!-#");
+                if (temCaracter == true) {
+                    break;
+                }
             }
-        } else {
-            System.out.printf("                 Login inválido");
-        }
+            String letraMaiuscula = String.valueOf(senha.charAt(cont));
+                if (letraMaiuscula.equals("A-Z")) {
+                    temMaiuscula = letraMaiuscula.equals("A-Z");
+                    if (temCaracter == true) {
+                        break;
+                    }
+                }
 
-    }
-}
+            String letraMinuscula = String.valueOf(senha.charAt(cont));
+                if (letraMinuscula.equals("a-z")) {
+                    temMinuscula = letraMinuscula.equals("a-z");
+                    if (temMinuscula == true) {
+                        break;
+                    }
+                }
+            String Numero = String.valueOf(senha.charAt(cont));
+                if (Numero.equals("0-9")) {
+                    temNumero = Numero.equals("0-9");
+                    if (temNumero == true) {
+                        break;
+                    }
+                }
+                cont++;
+            }
+
+                    System.out.printf("\n");
+
+                if(8 <= senha.length()) {
+                    if ((temCaracter = true) && (temMaiuscula = true) && (temMinuscula = true) && (temNumero = true)) {
+                        System.out.printf("                    Login criado com sucesso");
+                    }
+                    else{
+                        System.out.printf("         Digite uma senha de no mínimo 8 caracteres, que tenha pelo menos um numero,uma letra minuscula, uma letra maiuscula e um caractere especial ");
+                        String senha1 = scanner.nextLine();
+                        System.out.printf("                    Login criado com sucesso");
+                    }
+                }
+                else {
+                    System.out.printf("         Digite uma senha de no mínimo 8 caracteres, que tenha pelo menos um numero,uma letra minuscula, uma letra maiuscula e um caractere especial ");
+                    String senha1 = scanner.nextLine();
+                    System.out.printf("                    Login criado com sucesso");
+                }
+
+                    System.out.printf("\n");
+                    System.out.printf("\n");
+                    System.out.printf("---------------------------------------------------------------------");
+                    System.out.printf("\n");
+                    System.out.printf("                             Login");
+                    System.out.printf("\n");
+                    System.out.printf("---------------------------------------------------------------------");
+                    System.out.printf("\n");
+                    System.out.printf("\n");
+                    System.out.printf("            Digite seu Login: ");
+                    String log1 = scanner.nextLine();
+                    if (log.equals(log1)) {
+                        System.out.printf("\n");
+                        System.out.printf("            Digite sua senha: ");
+                        String senha1 = scanner.nextLine();
+                        System.out.printf("\n");
+                        if (senha.equals(senha1)) {
+                            System.out.printf("                 Login realizado com sucesso");
+                        } else {
+                            System.out.printf("            senha inválida");
+                        }
+                    } else {
+                        System.out.printf("                 Login inválido");
+                    }
+
+
+            }
+        }
